d3b46f065acf2c7462216b7446a0a0ac98695a5b
Barkhatov Andrey <barkhatovandrew@gmail.com>
2023-06-28 10:25:22 +0400

    install zustand, add store auth

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -7,6 +7,7 @@ import { RxCaretLeft, RxCaretRight } from 'react-icons/rx'
 import { HiHome } from 'react-icons/hi'
 import { BiSearch } from 'react-icons/bi'
 import { Button } from '@/components/Button'
+import { useAuthModal } from '@/hooks/use-auth-modal'
 
 interface HeaderProps {
   className?: string
@@ -16,6 +17,7 @@ interface HeaderProps {
 export const Header = (props: HeaderProps) => {
   const { children, className } = props
   const router = useRouter()
+  const { onOpen } = useAuthModal()
 
   const handleLogout = () => {
     //
@@ -27,14 +29,12 @@ export const Header = (props: HeaderProps) => {
         <div className="hidden items-center gap-x-2 md:flex">
           <button
             className="flex items-center justify-center rounded-full bg-black transition hover:opacity-75"
-            onClick={() => router.back()}
-          >
+            onClick={() => router.back()}>
             <RxCaretLeft className="text-white" size={35} />
           </button>
           <button
             className="flex items-center justify-center rounded-full bg-black transition hover:opacity-75"
-            onClick={() => router.forward()}
-          >
+            onClick={() => router.forward()}>
             <RxCaretRight className="text-white" size={35} />
           </button>
         </div>
@@ -48,10 +48,14 @@ export const Header = (props: HeaderProps) => {
         </div>
         <div className="flex items-center justify-between gap-x-4">
           <div>
-            <Button className="bg-transparent font-medium text-neutral-300">Sign up</Button>
+            <Button className="bg-transparent font-medium text-neutral-300" onClick={onOpen}>
+              Sign up
+            </Button>
           </div>
           <div>
-            <Button className="bg-white px-6 py-2 font-medium text-black">Log in</Button>
+            <Button className="bg-white px-6 py-2 font-medium text-black" onClick={onOpen}>
+              Log in
+            </Button>
           </div>
         </div>
       </div>
